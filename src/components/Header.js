b2ed82b24b02430d5e80c240c34de48817df4b31
Sami Keijonen <sami.keijonen@foxnet.fi>
2020-10-21 18:20:45 +0300

    Clean up.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,14 @@ function Header({order}) {
 
     const orderCount = Object.keys(order).length;
     const orderCountText = orderCount === 1 ? `- ${orderCount} item` : `- ${orderCount} items`;
+
     return (
         <header>
             <h1>Catch of the day</h1>
             <nav>
                 <ul>
                     <li><Link to={`${url}`}>Shop</Link></li>
-    <li><Link to={`${url}/checkout`}>Checkout</Link> <span>{orderCountText}</span></li>
+                    <li><Link to={`${url}/checkout`}>Checkout</Link> <span>{orderCountText}</span></li>
                     <li><Link to={`${url}/inventory`}>Inventory</Link></li>
                 </ul>
             </nav>
